5c73f644c8217d7774966c4e314ef18c414ad92d
André Monteiro <36992115+am0nt31r0@users.noreply.github.com>
2018-09-30 17:28:34 +0100

    Add files via upload

---
+++ dnsSniffer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from scapy.all import DNSQR, DNSRR, UDP, IP, DNS, sniff
+from datetime import datetime
+
+INTERFACE = "eth0" # interface that sniffs
+# number of sniffed queries, don't forget that for every query there is a response. Uncomment if you want to specify the number of DNS packets
+# NUMBER_QUERIES = 10
+
+def grep_DNS_queries(packet):
+	packet_time = packet.sprintf('%sent.time%')
+
+	try:
+		if DNSQR in packet and packet.dport == 53:
+			
+			print packet[DNS].summary() + '\n[' + packet[IP].src + '] -> [' + packet[IP].dst + '] at [' + packet_time + ']'
+
+		elif DNSRR in packet and packet.sport == 53:
+			
+			print packet[DNS].summary() + '\n['+ packet[IP].src + '] -> [' + packet[IP].dst + '] at [' + packet_time + ']'
+
+	except:
+		pass
+			
+# sniffs the packets
+packets = sniff(iface = INTERFACE, filter = "udp and port 53", store = 0, prn = grep_DNS_queries)#, count = NUMBER_QUERIES)
